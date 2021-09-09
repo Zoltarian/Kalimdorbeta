e174a9e468a8e5a3dd2420a1d90ee4a8124eb626
Zoltarian <Zoltarian@hotmail.com>
2021-09-09 19:37:22 -0400

    the shit

---
+++ kalimdorbeta/common/defines/HOA_defines.lua
@@ -0,0 +1,22 @@
+NDefines.NGame.START_DATE = "592.1.1.12"
+NDefines.NGame.END_DATE = "650.1.1.1"
+NDefines.NGame.BASE_STAGE_COUP_DAILY_PP = 1.5
+NDefines.NGame.IC_TO_EQUIPMENT_COUP_RATIO = 0.3
+
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_WIDTH = 82
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_HEIGHT = 129
+NDefines.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 2048
+NDefines.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512
+NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 10
+NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 8196
+NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_WIDTH = 41
+NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000
+
+NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 2.0	-- Scale to smaller unit sizes
+NDefines.NMilitary.FIELD_EXPERIENCE_SCALE = 0.045	-- Scale to smaller unit sizes
+
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 16 --24
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 16 --24
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 3 --5
+
+NDefines.NMilitary.BASE_FORT_PENALTY = -0.10 --Nerf defensive bonus, from -0.15
---
+++ kalimdorbeta/sound/voice_lines/readme.txt
@@ -0,0 +1,8 @@
+wc3 conversions
+What = idle
+Yes = move
+Yesattack & Warcry = attack
+
+
+converting ogg to wav
+https://online-audio-converter.com/
