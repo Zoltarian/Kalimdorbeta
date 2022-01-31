539becb373e793831adcda976df4611138b504b9
Zoltarian <Zoltarian@hotmail.com>
2022-01-31 00:26:22 -0500

    common/event changes

--- hoakalimdorbeta/common/defines/HOA_defines.lua
+++ hoakalimdorbeta/common/defines/HOA_defines.lua
@@ -18,3 +18,16 @@ NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 16 --24
 NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 3 --5
 
 NDefines.NMilitary.BASE_FORT_PENALTY = -0.10 --Nerf defensive bonus, from -0.15
+
+
+
+
+
+-- AI Diplomacy
+NDefines.NAI.GENERATE_WARGOAL_THREAT_BASELINE = 0.0
+NDefines.NAI.DIPLOMACY_SEND_MAX_FACTION = 0.5
+NDefines.NAI.FORCE_FACTOR_AGAINST_EXTRA_MINOR = 0.4			-- AI considers generating wargoals against minors below this % of force compared to themselves to get at a bigger enemy.
+NDefines.NAI.MAX_EXTRA_WARGOAL_GENERATION = 2				-- AI may want to generate wargoals against weak minors to get at larger enemy, but never more that this at any given time.
+NDefines.NAI.WARGOAL_GENERATION_STRENGTH_FACTOR = 1.5	-- Desire to generate wargoal effected negatevely if actor strength is less than this factor of target strength
+NDefines.NAI.DECLARE_WAR_RELATIVE_FORCE_FACTOR = 0.4	-- Weight of relative force between nations that consider going to war
+NDefines.NAI.DECLARE_WAR_NOT_NEIGHBOR_FACTOR = 0.25		-- Multiplier applied before force factor if country is not neighbor with the one it is considering going to war
